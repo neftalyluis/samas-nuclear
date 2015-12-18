d9def1aef73e654c06b05dc7353eb8301369fa2b
Neftaly <amaalanis@gmail.com>
2015-12-18 17:29:54 -0600

    + Creacion de BondCollateral y propiedades de Bond

--- ejb/src/main/java/mx/samas/ejb/entities/Bond.java
+++ ejb/src/main/java/mx/samas/ejb/entities/Bond.java
@@ -6,22 +6,39 @@
 package mx.samas.ejb.entities;
 
 import java.io.Serializable;
+import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Temporal;
 
 /**
  *
  * @author neftaly
  */
 @Entity
-public class Bond implements Serializable {
+public class Bond extends Asset implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    
+    private Double spreadFixed;
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date maturityDate;
+    private CashflowDate cashflowDates;
+    private DayCount dayCount;
+    private DenominatorCurrency currencyDenomination;
+    
+    /**
+     * Propiedades Distintivas
+     */
+    private Boolean callable;
+    private Boolean amortizing;
+    //Falta definir BondCollateral
+    private BondCollateral collateralized;
+    private TermStructure termStructure;
     
     
     public Long getId() {
@@ -55,6 +72,132 @@ public class Bond implements Serializable {
     @Override
     public String toString() {
         return "mx.samas.ejb.entities.Bond[ id=" + id + " ]";
+    }
+
+    /**
+     * @return the spreadFixed
+     */
+    public Double getSpreadFixed() {
+        return spreadFixed;
+    }
+
+    /**
+     * @param spreadFixed the spreadFixed to set
+     */
+    public void setSpreadFixed(Double spreadFixed) {
+        this.spreadFixed = spreadFixed;
+    }
+
+    /**
+     * @return the maturityDate
+     */
+    public Date getMaturityDate() {
+        return maturityDate;
+    }
+
+    /**
+     * @param maturityDate the maturityDate to set
+     */
+    public void setMaturityDate(Date maturityDate) {
+        this.maturityDate = maturityDate;
+    }
+
+    /**
+     * @return the cashflowDates
+     */
+    public CashflowDate getCashflowDates() {
+        return cashflowDates;
+    }
+
+    /**
+     * @param cashflowDates the cashflowDates to set
+     */
+    public void setCashflowDates(CashflowDate cashflowDates) {
+        this.cashflowDates = cashflowDates;
+    }
+
+    /**
+     * @return the dayCount
+     */
+    public DayCount getDayCount() {
+        return dayCount;
+    }
+
+    /**
+     * @param dayCount the dayCount to set
+     */
+    public void setDayCount(DayCount dayCount) {
+        this.dayCount = dayCount;
+    }
+
+    /**
+     * @return the currencyDenomination
+     */
+    public DenominatorCurrency getCurrencyDenomination() {
+        return currencyDenomination;
+    }
+
+    /**
+     * @param currencyDenomination the currencyDenomination to set
+     */
+    public void setCurrencyDenomination(DenominatorCurrency currencyDenomination) {
+        this.currencyDenomination = currencyDenomination;
+    }
+
+    /**
+     * @return the callable
+     */
+    public Boolean getCallable() {
+        return callable;
+    }
+
+    /**
+     * @param callable the callable to set
+     */
+    public void setCallable(Boolean callable) {
+        this.callable = callable;
+    }
+
+    /**
+     * @return the amortizing
+     */
+    public Boolean getAmortizing() {
+        return amortizing;
+    }
+
+    /**
+     * @param amortizing the amortizing to set
+     */
+    public void setAmortizing(Boolean amortizing) {
+        this.amortizing = amortizing;
+    }
+
+    /**
+     * @return the collateralized
+     */
+    public BondCollateral getCollateralized() {
+        return collateralized;
+    }
+
+    /**
+     * @param collateralized the collateralized to set
+     */
+    public void setCollateralized(BondCollateral collateralized) {
+        this.collateralized = collateralized;
+    }
+
+    /**
+     * @return the termStructure
+     */
+    public TermStructure getTermStructure() {
+        return termStructure;
+    }
+
+    /**
+     * @param termStructure the termStructure to set
+     */
+    public void setTermStructure(TermStructure termStructure) {
+        this.termStructure = termStructure;
     }
     
 }
--- ejb/src/main/java/mx/samas/ejb/entities/BondCollateral.java
+++ ejb/src/main/java/mx/samas/ejb/entities/BondCollateral.java
@@ -16,10 +16,10 @@ import javax.persistence.Id;
  * @author neftaly
  */
 @Entity
-public class Derivative extends Asset implements Serializable {
+public class BondCollateral implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     public Long getId() {
@@ -40,10 +40,10 @@ public class Derivative extends Asset implements Serializable {
     @Override
     public boolean equals(Object object) {
         // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Derivative)) {
+        if (!(object instanceof BondCollateral)) {
             return false;
         }
-        Derivative other = (Derivative) object;
+        BondCollateral other = (BondCollateral) object;
         if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
             return false;
         }
@@ -52,7 +52,7 @@ public class Derivative extends Asset implements Serializable {
 
     @Override
     public String toString() {
-        return "mx.samas.ejb.entities.Derivative[ id=" + id + " ]";
+        return "mx.samas.ejb.entities.BondCollateral[ id=" + id + " ]";
     }
     
 }
