0602ab8f021204afea88dc3682f4bacaaf7569f8
Abimael Luis <abi@airmail.cc>
2016-06-23 13:41:50 -0500

    And it goes And it goes, deployed?

--- src/main/java/mx/samas/controller/HelloController.java
+++ src/main/java/mx/samas/controller/HelloController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 public class HelloController {
 
-    @RequestMapping("/")
+    @RequestMapping("/hello")
     public String index() {
         return "Greetings from SAMAS, And Openshift";
     }
