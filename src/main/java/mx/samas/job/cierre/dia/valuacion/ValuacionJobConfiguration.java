d1bd0448d967900111a8cb840373ffbeb0b2e16c
Abimael Luis <abimael@samas.mx>
2016-09-22 06:44:01 -0500

    Agregamos TyC de PIP y VectorValmer

--- src/main/java/mx/samas/job/cierre/dia/valuacion/ValuacionJobConfiguration.java
+++ src/main/java/mx/samas/job/cierre/dia/valuacion/ValuacionJobConfiguration.java
@@ -11,7 +11,6 @@ import mx.samas.domain.VectorActivo;
 import mx.samas.domain.dto.VectorActivoDTO;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
