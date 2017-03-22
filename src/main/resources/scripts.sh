a9a36486d13c1fe4a6102dca9925c0371bf6e2fb
Abimael Luis <abimael@samas.mx>
2017-03-22 12:03:11 -0600

    Cleaning liquibase and quartz props

--- src/main/resources/scripts.sh
+++
@@ -1,8 +0,0 @@
-./liquibase --driver=com.mysql.jdbc.Driver \
-        --url=jdbc:mysql://localhost:3306/samas?useSSL=false  \
-        --username=samas \
-        --password=test \
-    diffChangeLog \
-        --referenceUrl=jdbc:mysql://samas.cllibwzh1wfc.us-west-2.rds.amazonaws.com:3306/samas?useSSL=false  \
-        --referenceUsername=samas \
-        --referencePassword=Abcd1234#
